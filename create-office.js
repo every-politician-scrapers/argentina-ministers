1e611dbebdac6e755f9decdc883fdf8aefa544e0
Tony Bowden <tony@tmtm.com>
2022-04-04 08:26:18 +0300

    update to latest wikibase-cli scripts

--- create-office.js
+++ create-office.js
@@ -9,7 +9,7 @@ module.exports = (label, ministry) => {
     P279:  { value: 'Q83307'  }, // subclas of: minister
     P17:   { value: meta.country ? meta.country.id : meta.jurisdiction.id },
     P1001: { value: meta.jurisdiction.id },
-    P361:  { value: meta.cabinet.parent },
+    P361:  { value: meta.cabinet.parent }
   }
 
   if(ministry)   claims['P2389']  = ministry
